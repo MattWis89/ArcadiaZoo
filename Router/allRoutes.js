24bd5321f5b078b17302691b68c3c1fbdd13a121
Matt <matthieu_wis@hotmail.com>
2024-07-18 18:46:19 +0200

    ajout page contact

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -5,7 +5,9 @@ export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/Services", "Services", "/pages/services.html"),
     new Route("/Habitats","Habitats", "/pages/habitats.html"),
-    new Route("/Signin","Connexion", "/pages/signin.html")
+    new Route("/Signin","Connexion", "/pages/signin.html"),
+    new Route("/Account", "Mon compte", "/pages/account.html"),
+    new Route("/Contact", "Contact", "/pages/contact.html")
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
